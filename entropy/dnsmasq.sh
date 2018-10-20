c85f9fa8dab6b63753c8e7905bddb24bd912f568
Ammon Casey <ammon@caseyohana.com>
2018-10-19 16:20:40 -1000

    renamed dnsmasq config to homestead.conf

--- entropy/dnsmasq.sh
+++ entropy/dnsmasq.sh
@@ -3,7 +3,7 @@
 
 TLD=`echo $2 | sed -e "s/[^.]*]\?\([^.]*\)/\1/" | cut -d'.' -f 2`
 
-sed -i "s/listen-address=192.168.10.10/listen-address=$1/" /etc/dnsmasq.d/entropy.conf
+sed -i "s/listen-address=192.168.10.10/listen-address=$1/" /etc/dnsmasq.d/homestead.conf
 echo -e "$1 $2." >> /etc/hosts.dnsmasq
 echo -e "domain=$TLD" > "/etc/dnsmasq.d/$TLD.conf"
 echo -e "local=/$TLD/" >> "/etc/dnsmasq.d/$TLD.conf"
