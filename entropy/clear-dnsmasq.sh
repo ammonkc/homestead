5d2a51c3d98326f40243d6ee08ff51a3716c0e37
Ammon Casey <ammon@caseyohana.com>
2018-10-23 10:41:53 -1000

    [dnsmasq] made the dnsmasq script more robust to be able to properly configure any private ip address and not just the default homestead ip

--- entropy/clear-dnsmasq.sh
+++ entropy/clear-dnsmasq.sh
@@ -3,3 +3,5 @@
 # Clear The Old Dnsmasq records
 
 > /etc/hosts.dnsmasq
+
+find /etc/dnsmasq.d/ -type f -not -name 'homestead.conf' -delete
--- entropy/dnsmasq.sh
+++ entropy/dnsmasq.sh
@@ -3,7 +3,7 @@
 
 TLD=`echo $2 | sed -e "s/[^.]*]\?\([^.]*\)/\1/" | cut -d'.' -f 2`
 
-sed -i "s/listen-address=192.168.10.10/listen-address=$1/" /etc/dnsmasq.d/homestead.conf
+sed -i -r "s/listen-address=(192\.|172\.|10\.)(\b[0-9]{1,3}\.){2}[0-9]{1,3}\b/listen-address=$1/" /etc/dnsmasq.d/homestead.conf
 echo -e "$1 $2." >> /etc/hosts.dnsmasq
 echo -e "domain=$TLD" > "/etc/dnsmasq.d/$TLD.conf"
 echo -e "local=/$TLD/" >> "/etc/dnsmasq.d/$TLD.conf"
