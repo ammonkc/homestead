d73e17b4f8be64c3ba56491e1d3d2767c27f85bd
Ammon Casey <acasey@panda-group.com>
2018-10-26 14:32:50 -1000

    Add support for wildcard subdomains on configured dnsmasq domains

--- entropy/dnsmasq.sh
+++ entropy/dnsmasq.sh
@@ -5,5 +5,8 @@ TLD=`echo $2 | sed -e "s/[^.]*]\?\([^.]*\)/\1/" | cut -d'.' -f 2`
 
 sed -i -r "s/listen-address=(192\.|172\.|10\.)(\b[0-9]{1,3}\.){2}[0-9]{1,3}\b/listen-address=$1/" /etc/dnsmasq.d/homestead.conf
 echo -e "$1 $2." >> /etc/hosts.dnsmasq
-echo -e "domain=$TLD" > "/etc/dnsmasq.d/$TLD.conf"
-echo -e "local=/$TLD/" >> "/etc/dnsmasq.d/$TLD.conf"
+cat << DOMAIN_EOF > /etc/dnsmasq.d/$2.conf
+domain=$TLD
+local=/$TLD/
+address=/$2/$1
+DOMAIN_EOF
