e7e02923f12265474358e89898c10368d4fd6e60
Ammon Casey <acasey@panda-group.com>
2018-10-30 09:46:29 -1000

    Fix dnsmasq records to work properly with wildcard subdomains

--- entropy/dnsmasq.sh
+++ entropy/dnsmasq.sh
@@ -6,7 +6,5 @@ TLD=`echo $2 | sed -e "s/[^.]*]\?\([^.]*\)/\1/" | cut -d'.' -f 2`
 sed -i -r "s/listen-address=(192\.|172\.|10\.)(\b[0-9]{1,3}\.){2}[0-9]{1,3}\b/listen-address=$1/" /etc/dnsmasq.d/homestead.conf
 echo -e "$1 $2." >> /etc/hosts.dnsmasq
 cat << DOMAIN_EOF > /etc/dnsmasq.d/$2.conf
-domain=$TLD
-local=/$TLD/
 address=/$2/$1
 DOMAIN_EOF
